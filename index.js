4b35cb61ab26aabe1a5a5e7c2be1a49afacd6459
I-Shadow-I <57918832+I-Shadow-I@users.noreply.github.com>
2020-05-21 16:59:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
-var config = require('./config.json');
+var config = require('/config.json');
 const Client = new Discord.Client();
 const fs = require('fs');
 const Canvas = require('canvas');
 const Snekfetch = require('snekfetch');
-let xp = JSON.parse(fs.readFileSync('./xp/xp.json', 'utf8'));
-let xp_lol = JSON.parse(fs.readFileSync('./xp/xp_lol.json', 'utf8'));
-let xp_ftn = JSON.parse(fs.readFileSync('./xp/xp_ftn.json', 'utf8'));
-let xp_rl = JSON.parse(fs.readFileSync('./xp/xp_rl.json', 'utf8'));
-let xp_cs = JSON.parse(fs.readFileSync('./xp/xp_cs.json', 'utf8'));
-let xp_min = JSON.parse(fs.readFileSync('./xp/xp_min.json', 'utf8'));
-let xp_war = JSON.parse(fs.readFileSync('./xp/xp_war.json', 'utf8'));
+let xp = JSON.parse(fs.readFileSync('/xp/xp.json', 'utf8'));
+let xp_lol = JSON.parse(fs.readFileSync('/xp/xp_lol.json', 'utf8'));
+let xp_ftn = JSON.parse(fs.readFileSync('/xp/xp_ftn.json', 'utf8'));
+let xp_rl = JSON.parse(fs.readFileSync('/xp/xp_rl.json', 'utf8'));
+let xp_cs = JSON.parse(fs.readFileSync('/xp/xp_cs.json', 'utf8'));
+let xp_min = JSON.parse(fs.readFileSync('/xp/xp_min.json', 'utf8'));
+let xp_war = JSON.parse(fs.readFileSync('/xp/xp_war.json', 'utf8'));
 var prefix = config.Prefix;
 
 Client.login(process.env.TOKEN);
@@ -489,7 +489,7 @@ Client.on('message', async message => {
         const canvas = Canvas.createCanvas(1004, 720);
         const ctx = canvas.getContext("2d");
 
-        const background = await Canvas.loadImage("./img/fond-xp.png");
+        const background = await Canvas.loadImage("/img/fond-xp.png");
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         //bordure
